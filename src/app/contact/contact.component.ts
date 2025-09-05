16044ab4eb4ae02874083812f9f3e31f2163eb97
lautaro avila <l4vil4096@gmail.com>
2025-09-04 22:22:19 -0300

    first commit3

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -14,31 +14,24 @@ export class ContactComponent {
 
    constructor() {
     // Inicializa EmailJS con tu clave pública
-    emailjs.init('EC21HTNE0PH36sjXt');
+    // emailjs.init('EC21HTNE0PH36sjXt');
   }
 
-  sendEmail() {
-    if (!this.nombre || !this.email || !this.mensaje) {
-      alert('Por favor completa todos los campos.');
-      return;
-    }
-
-    const templateParams = {
-      from_name: this.nombre,
-      from_email: this.email,
-      message: this.mensaje
-    };
-
-    emailjs.send('service_6nqeqim', 'template_6zjqqem', templateParams)
-      .then((response) => {
-        console.log('Correo enviado!', response.status, response.text);
-        alert('Mensaje enviado correctamente.');
-        this.nombre = '';
-        this.email = '';
-        this.mensaje = '';
-      }, (err) => {
-        console.error('Error al enviar el correo:', err);
-        alert('Ocurrió un error, intenta nuevamente.');
-      });
+sendEmail() {
+  if (!this.nombre || !this.email || !this.mensaje) {
+    alert('Por favor completa todos los campos.');
+    return;
   }
+
+  const destinatario = "lautiavila96@gmail.com";
+  const asunto = encodeURIComponent("Nuevo mensaje de contacto");
+  const cuerpo = encodeURIComponent(
+    `\n${this.mensaje}`
+  );
+
+  const url = `https://mail.google.com/mail/?view=cm&fs=1&to=${destinatario}&su=${asunto}&body=${cuerpo}`;
+
+  window.open(url, "_blank"); // 👈 abre Gmail en una pestaña nueva
+}
+
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-header',
@@ -11,4 +12,24 @@ export class HeaderComponent {
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
   }
+
+   mostrarCV() {
+  Swal.fire({
+    title: 'Currículum',
+    text: 'Haz click aquí para descargar mi CV en formato PDF',
+    icon: 'info',
+    showCancelButton: true,
+    confirmButtonText: '📄 Descargar',
+    cancelButtonText: '❌ Cancelar',
+    confirmButtonColor: '#06b6d4',
+  }).then((result) => {
+    if (result.isConfirmed) {
+      const link = document.createElement('a');
+      link.href = './../../assets/Lautaro Avila ult Act CV.pdf';   // ruta de tu CV
+      link.download = 'Curriculum_Lautaro_Avila.pdf'; // nombre con el que se guarda
+      link.click();
+    }
+  });
+}
+
 }
